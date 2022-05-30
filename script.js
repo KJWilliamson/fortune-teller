cee105044e34d8432f04ba9480d281e9801b48de
kamela williamson <kami63116@yahoo.com>
2022-05-29 21:52:18 -0700

    changed fortunes

--- script.js
+++ script.js
@@ -4,24 +4,30 @@ let modal = document.querySelector("#modal");
 button.addEventListener("click", function() {
 
 
-let randomNum = Math.floor( Math.random() * 6 ) +1;
+let randomNum = Math.floor( Math.random() * 7 ) +1;
 let answers = "";
   switch (randomNum) {
   case 1: 
-    answers = "Yes"; 
+    answers = "You will embark <br>on a long trip<br> very soon"; 
     break; 
   case 2:
-    answers = "No";
+    answers = "You will receive<br> wonderful news <br>next month";
     break; 
   case 3:
-    answers = "Not likely"; 
+    answers = "Be patient.<br> It's not time."; 
     break;
   case 4: 
-    answers = "It will be so";
+    answers = "Ask for that <br>promotion";
+    break;
   case 5:
-    answers = "The future is yours";
+    answers = "Buy a lottery <br>ticket on the <br>next Saturday.";
+    break;
   case 6: 
-    answers = "It's very likely";
+    answers = "Join that gym <br>you drive past <br>everyday.";
+    break;
+  case 7:
+    answers = "Stop worrying<br> and just ask.";
+    break;
   }
   modal.innerHTML = answers;
 
